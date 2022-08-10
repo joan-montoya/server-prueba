ace3cad45517f0678b69065af7f497c5d13dedc8
cecilia-tisca <ceciliatisca@gmail.com>
2022-08-10 02:06:57 -0500

    Feat: All the dependencies that are going to be used are called and the URL of our mongo was put

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const http = require("http");
+const servidor = http.createServer(app);
+
+const JWT_SECRET =
+  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+
+const mongoUrl =
+  "mongodb+srv://admin:3526@cluster0.4pvv9.mongodb.net/login";
