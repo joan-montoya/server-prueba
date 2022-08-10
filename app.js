3de0af7a01dc201b88adef705936b80549fdc491
Karla Moriana Rodriguez Yllescas <moriAeseces@gmail.com>
2022-08-10 03:19:18 -0500

    feat: Socket functionality on the server

--- app.js
+++ app.js
@@ -81,3 +81,38 @@ app.post("/userData", async (req, res) => {
       });
   } catch (error) {}
 });
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
+
+const socketio = require("socket.io");
+const io = socketio(servidor);
+
+//Funcionalidad de socket.io en el servidor
+io.on("connection", (socket) => {
+  let nombre;
+
+  socket.on("conectado", (nomb) => {
+    nombre = nomb;
+    //socket.broadcast.emit manda el mensaje a todos los clientes excepto al que ha enviado el mensaje
+    socket.broadcast.emit("mensajes", {
+      nombre: nombre,
+      mensaje: `${nombre} ha entrado en la sala del chat`,
+    });
+  });
+
+  socket.on("mensaje", (nombre, mensaje) => {
+    //io.emit manda el mensaje a todos los clientes conectados al chat
+    io.emit("mensajes", { nombre, mensaje });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("mensajes", {
+      servidor: "Servidor",
+      mensaje: `${nombre} ha abandonado la sala`,
+    });
+  });
+});
+
+servidor.listen(5000, () => console.log("Servidor inicializado"));
