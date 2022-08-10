193ae72c8d3913c388c8624482178c5fc4e97c94
Karla Moriana Rodriguez Yllescas <moriAeseces@gmail.com>
2022-08-10 02:47:28 -0500

    feat: the project is connected yo the database

--- app.js
+++ app.js
@@ -14,3 +14,36 @@ const JWT_SECRET =
 
 const mongoUrl =
   "mongodb+srv://admin:3526@cluster0.4pvv9.mongodb.net/login";
+  mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+
+require("./userDetails");
+
+const User = mongoose.model("UserInfo");
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.json({ error: "User Exists" });
+    }
+    await User.create({
+      fname,
+      lname,
+      email,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
